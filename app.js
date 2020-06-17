f450af7236ce4ec2e4f913ae44319b3821afb9d8
Mario Campaz <mcampaz@hotmail.com>
2020-06-17 08:24:01 -0700

    add cors

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
 const { ApolloServer } = require('apollo-server-express');
+const cors = require('cors');
 
 const models = require('./models');
 const typeDefs = require('./typeDefs');
-const resolvers = require('./resolvers')
+const resolvers = require('./resolvers');
 
 const server = new ApolloServer({
     typeDefs,
@@ -14,6 +15,7 @@ const server = new ApolloServer({
     }
 });
 server.applyMiddleware({ app });
+app.use(cors());
 
 app.listen(3000, () => console.info("Apollo graphQL server is running on port 3000"));
 
